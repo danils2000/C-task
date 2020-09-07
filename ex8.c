cd1fedd565d41bf6d9929a08a39e19cf9c301c21
Danil Sisov <56063160+danils2000@users.noreply.github.com>
2020-09-07 17:12:41 +0300

    Update ex8.c

--- ex8.c
+++ ex8.c
@@ -6,12 +6,12 @@ void convert(char *something) {
 	for(int i = 0; something[i]!='\0'; i++) {
 		if (i == 0) {
 			if((something[i] >= 'a' && something[i] <= 'z'))
-                something[i] = toupper(something[i]);
+                	something[i] = toupper(something[i]);
 		}
 		if (something[i] == ' ') {
 			i++;
 			if(something[i] >= 'a' && something[i] <= 'z') {
-				something[i] = toupper(something[i]);
+			something[i] = toupper(something[i]);
 			}
 		}
 	}
